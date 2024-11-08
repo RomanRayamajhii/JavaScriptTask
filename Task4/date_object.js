97de4a3bed5c156e2832fc31fb975138597a569d
RomanRayamajhii <romanraya4@gmail.com>
2024-11-09 00:43:58 +0545

    Date work in js

---
+++ Task4/date_object.js
@@ -0,0 +1,10 @@
+let date =new Date();
+console.log(date);
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDate());
+console.log(date.getHours());
+// Formating or changing date and time own self
+// let date1 = new Date('2024-03-20T10:30:00'); or
+let date1= new Date(2081,7,26,11,43,20);
+console.log(date1);
