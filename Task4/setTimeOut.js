2184f229be8ee1fe5e22a452b880464e9d251f06
RomanRayamajhii <romanraya4@gmail.com>
2024-11-09 00:56:29 +0545

    SetTimeOut in js

---
+++ Task4/setTimeOut.js
@@ -0,0 +1,14 @@
+function print(){
+    console.log("my name is roman");
+
+}
+print();
+setTimeout(function(){
+    console.log(" I am programmer .");
+},3000);
+// Example 2
+function display(){
+    window.alert("hello roman");
+   
+}
+setTimeout(display,3000);
