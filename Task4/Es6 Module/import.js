fb21928e1e506fffbc75e50fa55ccd6f49640345
RomanRayamajhii <romanraya4@gmail.com>
2024-11-09 14:12:12 +0545

    Es6 module in js

--- Task4/Es6 Module/import.js
+++ Task4/Es6 Module/import.js
@@ -5,4 +5,4 @@ console.log("PI:", PI);                    // Output: 3.14
 console.log("Addition:", add(5, 7));       // Output: 12
 console.log("Subtraction:", sub(10, 3));   // Output: 7
 console.log("Area:", getArea(5));          // Output: 78.5
-console.log("Circumference:", getCircumference(5)); // Output: 31.4
+console.log("Circumference:", getCircumference(5).toFixed(2)); // Output: 31.4
